624d92e2caf998fcfd15049451fe44a4a0aaa29f
kazuhiro maeda <kazurou147@gmail.com>
2021-09-13 18:43:25 +0900

    初回

---
+++ template/post-single.php
@@ -0,0 +1,15 @@
+<!-- ブログ　個別記事  -->
+
+<div class="l-post-info">
+  <span class="l-post-into_time">
+    <time datetime="<?php echo esc_html(get_the_date('c')); ?>">
+      <?php echo esc_html(get_the_date()); ?></time>
+  </span>
+</div>
+
+<h2><?php the_title();  ?></h2>
+
+
+<div class="l-blog-content">
+  <?php the_content(); ?>
+</div>
